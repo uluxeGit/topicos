06ae19ec1d9be5cf100ab3206cbff06186531d91
uluxeGit <al22760594@ite.edu.mx>
2024-02-25 19:02:25 -0800

    se agrego la funcionalidad de que el color no se repita y se asignaron 20 colores para elegir fondo

--- lecture02/main2.js
+++ lecture02/main2.js
@@ -1,12 +1,23 @@
 const body = document.querySelector('body');
-const colores = ['red', 'blue', 'green', 'pink', 'orange', 
-                'yellow', 'gray', 'silver', 'purple', 'cyan', 
+const colores = ['Red', 'Blue', 'Green', 'Pink', 'Orange', 
+                'Yellow', 'Gray', 'Silver', 'Purple', 'Cyan', 
+                'DarkMagenta', 'White', 'Aqua', 'Violet','Beige', 
+                'Mustard', 'Turquoise', 'Brown','Lime', 'Terracotta',
 ];
+const coloresYaUsados = [];
 
-for(let i=0;i<10;i++){
+
+for(let i = 0; i < 10; i++){
     const div = document.createElement('div');
+    let colorParaElFondo;
     div.style.height = '100px';
-    div.style.backgroundColor = colores[Math.floor(Math.random()*colores.length)];
+    //genero los colores de forma random
+    do{
+        colorParaElFondo = colores[Math.floor(Math.random()*colores.length)];
+    }while(coloresYaUsados.includes(colorParaElFondo));
 
+    coloresYaUsados.push(colorParaElFondo);
+
+    div.style.backgroundColor = colorParaElFondo;
     body.appendChild(div);
 }
